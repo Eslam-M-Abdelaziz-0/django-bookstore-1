6441672888610a054a67a619fb5ec7087e36315c
Nikita Mochalov <mochalov.n@ya.ru>
2018-08-08 20:31:18 +0300

    Added index page

--- market/views.py
+++ market/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book, Category
 
 # Create your views here.
+
+def index_page(request):
+    return render(
+        request,
+        "market/index.html",
+        dict(
+            popular_books=Book.objects.all()[:5],
+            categories=[Category.objects.all()[i::3] for i in range(3)]
+        )
+    )
